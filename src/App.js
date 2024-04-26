8b0718c1229bd3e61eaf58d4a1a7bf504b140732
pkihiu <p.kihiu@alustudent.com>
2024-04-26 16:25:30 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Componets/Navbar";
 import Hero from "./Componets/Hero";
+import FAQ from "./Componets/FAQ";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <FAQ />
     </div>
   );
 }
